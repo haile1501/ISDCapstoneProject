42ea21d1fc45f4ac0f3d24d025da7c88755b843b
OOP-20212-20205181-TranDucDuy <103793658+OOP-20212-20205181-TranDucDuy@users.noreply.github.com>
2023-08-12 17:35:00 +0700

    bike sql

--- Code/ecobike/src/main/java/itss/ecobike/models/Bike.java
+++ Code/ecobike/src/main/java/itss/ecobike/models/Bike.java
@@ -1,4 +1,57 @@
 package itss.ecobike.models;
 
 public class Bike {
+    private String barcode;
+    private int battery_percentage;
+    private int dock_id;
+    private String license_plate;
+    private int bike_type_id;
+
+    public Bike(String barcode, int battery_percentage, int dock_id, String license_plate, int bike_type_id) {
+        this.barcode = barcode;
+        this.battery_percentage = battery_percentage;
+        this.dock_id = dock_id;
+        this.license_plate = license_plate;
+        this.bike_type_id = bike_type_id;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public int getBattery_percentage() {
+        return battery_percentage;
+    }
+
+    public void setBattery_percentage(int battery_percentage) {
+        this.battery_percentage = battery_percentage;
+    }
+
+    public int getDock_id() {
+        return dock_id;
+    }
+
+    public void setDock_id(int dock_id) {
+        this.dock_id = dock_id;
+    }
+
+    public String getLicense_plate() {
+        return license_plate;
+    }
+
+    public void setLicense_plate(String license_plate) {
+        this.license_plate = license_plate;
+    }
+
+    public int getBike_type_id() {
+        return bike_type_id;
+    }
+
+    public void setBike_type_id(int bike_type_id) {
+        this.bike_type_id = bike_type_id;
+    }
 }
--- Code/ecobike/src/main/java/itss/ecobike/models/BikeDAO.java
+++ Code/ecobike/src/main/java/itss/ecobike/models/BikeDAO.java
@@ -1,4 +1,54 @@
 package itss.ecobike.models;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import itss.ecobike.utils.DBUtil;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class BikeDAO {
+
+    public static ObservableList<Bike> searchBikes() throws SQLException, ClassNotFoundException {
+        String selectStmt = "SELECT * FROM bike";
+
+        try {
+            ResultSet rsBikes = DBUtil.dbExecuteQuery(selectStmt);
+
+            return getBikeList(rsBikes);
+        } catch (SQLException e) {
+            System.out.println("SQL select operation has been failed: " + e);
+            // Return exception
+            throw e;
+        }
+    }
+
+    public static ObservableList<Bike> getBikeByBarCode(String Barcode) throws SQLException, ClassNotFoundException {
+        String selectStmt = "SELECT * FROM bike WHERE barcode = "+ Barcode;
+        try {
+            ResultSet rsBikes = DBUtil.dbExecuteQuery(selectStmt);
+
+            return getBikeList(rsBikes);
+        } catch (SQLException e) {
+            System.out.println("SQL select operation has been failed: " + e);
+            // Return exception
+            throw e;
+        }
+    }
+    private static ObservableList<Bike> getBikeList(ResultSet rs) throws SQLException, ClassNotFoundException {
+        ObservableList<Bike> bikeList = FXCollections.observableArrayList();
+
+        while (rs.next()) {
+            Bike bike = new Bike(
+                    rs.getString("barcode"),
+                    rs.getInt("battery_percentage"),
+                    rs.getInt("dock_id"),
+                    rs.getString("license_plate"),
+                    rs.getInt("bike_type_id")
+            );
+            bikeList.add(bike);
+        }
+        // Return bikeList (ObservableList of Bikes)
+        return bikeList;
+    }
 }
